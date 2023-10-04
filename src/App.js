643817015d23b69a1c5f8cdcf6f932a9d4006137
Suchan Park <coolsu92@gmail.com>
2023-10-04 17:38:13 +1300

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './style.css';
+
+function App() {
+  const appTitle = "Today I learned";
+
+  return (
+  <>
+  {/* HEADER */}
+  <header className="header">
+  <div className="logo">
+    <img src="logo.png"
+      height="68"
+      width="68"
+      alt="Today I Learned Logo"
+    />
+    <h1>{appTitle}</h1>
+  </div>
+
+  <button className="btn btn-large btn-open">Share a fact</button>
+  </header>
+
+  <NewFactForm />
+  <main className='main'>
+
+  <CategoryFilter />
+  <FactList />
+  </main>
+  </>
+  
+);
+}
+
+function NewFactForm(){
+  return <form className='fact-form'>Fact form</form>
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList(){
+  return <section>Fact list</section>;
+}
+
+export default App;
