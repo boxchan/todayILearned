c5c029aaa82ddc0eceb5f5effa1d36a26aaa1edb
Suchan Park <coolsu92@gmail.com>
2023-10-11 18:43:04 +1300

    build

--- src/App.js
+++ src/App.js
@@ -130,9 +130,9 @@ function isValidHttpUrl(string) {
 
 
 function NewFactForm({setFacts, setShowForm}){
-  const [text, setText] = useState("");
-  const [source, setSource] = useState("http://example.com");
-  const [category, setCategory] = useState("");
+  const [text, setText] = useState('');
+  const [source, setSource] = useState('');
+  const [category, setCategory] = useState('');
   const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
 
@@ -222,6 +222,7 @@ function FactList({facts, setFacts}){
 
 function Fact({fact, setFacts}) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed = fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName){
     setIsUpdating(true);
@@ -234,6 +235,7 @@ function Fact({fact, setFacts}) {
 
   return       <li className="fact">
   <p>
+    {isDisputed ? <span className='disputed'>[DISPUTED🥹]</span> : null}
     {fact.text}
     <a className="source"
       href={fact.source}
